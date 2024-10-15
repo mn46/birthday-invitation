cd7c498e4928b972f86c1114ff98fbc7f1d96499
marta <m.nowak033@gmail.com>
2024-10-15 20:04:07 +0200

    Add basic navigation

--- app/(components)/Navigation.jsx
+++ app/(components)/Navigation.jsx
@@ -1,5 +1,19 @@
+import Link from "next/link";
+
 const Navigation = () => {
-  return <div>Navigation</div>;
+  return (
+    <ul>
+      <li>
+        <a href="#details">details</a>
+      </li>
+      <li>
+        <a href="#contact">contact</a>
+      </li>
+      <li>
+        <Link href="/wishlist">wishlist</Link>
+      </li>
+    </ul>
+  );
 };
 
 export default Navigation;
