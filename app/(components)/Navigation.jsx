659e28953f3a6db91f473355c2f01646557d14cd
marta <m.nowak033@gmail.com>
2024-10-15 20:07:59 +0200

    Style the navigation

--- app/(components)/Navigation.jsx
+++ app/(components)/Navigation.jsx
@@ -2,14 +2,14 @@ import Link from "next/link";
 
 const Navigation = () => {
   return (
-    <ul>
-      <li>
+    <ul className="mx-10 my-6 flex gap-10 justify-end">
+      <li className="nav-link">
         <a href="#details">details</a>
       </li>
-      <li>
+      <li className="nav-link">
         <a href="#contact">contact</a>
       </li>
-      <li>
+      <li className="nav-link">
         <Link href="/wishlist">wishlist</Link>
       </li>
     </ul>
