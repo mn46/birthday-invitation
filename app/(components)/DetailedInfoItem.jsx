8cef69e6a098febc48a8626ebfee9d47d95c7e57
marta <m.nowak033@gmail.com>
2024-10-19 12:42:29 +0200

    Add and display detailed info

--- app/(components)/DetailedInfoItem.jsx
+++ app/(components)/DetailedInfoItem.jsx
@@ -1,8 +1,8 @@
-const DetailedInfoItem = ({ title, text }) => {
+const DetailedInfoItem = ({ title, content }) => {
   return (
-    <div className="border-2 border-primary rounded-lg w-max px-6 py-4">
-      <h3 className="font-italiana text-center mb-4">Detailed info title</h3>
-      <p className="font-instrument-sans text-center">Detailed info</p>
+    <div className="border-2 border-primary rounded-lg px-6 py-4 flex-grow">
+      <h3 className="font-italiana text-center mb-4">{title}</h3>
+      <p className="font-instrument-sans text-center">{content}</p>
     </div>
   );
 };
--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,12 @@ import DetailedInfoItem from "./(components)/DetailedInfoItem";
 import Navigation from "./(components)/Navigation";
 
 const Home = () => {
+  const details = [
+    { title: "Day & Time", content: "16/11/2024 16:00" },
+    { title: "Address", content: "24 Sunflower st., 1234 Flowertown" },
+    { title: "Dresscode", content: "Casual and comfy" },
+  ];
+
   return (
     <div className="main-content bg-center bg-cover bg-no-repeat">
       <nav>
@@ -16,8 +22,14 @@ const Home = () => {
           <Counter />
         </section>
 
-        <section id="details" className="bg-white py-20">
-          <DetailedInfoItem />
+        <section id="details" className="bg-white py-20 px-36 flex gap-10">
+          {details.map((detail) => (
+            <DetailedInfoItem
+              key={detail.title}
+              title={detail.title}
+              content={detail.content}
+            />
+          ))}
         </section>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
