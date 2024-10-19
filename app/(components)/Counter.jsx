5cc7d92460680d2ce10ba87d2a42433b5c46adeb
marta <m.nowak033@gmail.com>
2024-10-19 10:01:50 +0200

    Create a counter

---
+++ app/(components)/Counter.jsx
@@ -0,0 +1,59 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+
+const Counter = () => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+  const birthdayDate = new Date("12.13.2024");
+
+  const today = new Date();
+
+  const [difference, setDifference] = useState(birthdayDate - today);
+
+  useEffect(() => {
+    const interval = setInterval(
+      () => setDifference((prev) => prev - 1000),
+      1000
+    );
+    return () => clearInterval(interval);
+  }, [difference]);
+
+  let days = 0;
+  let hours = 0;
+  let minutes = 0;
+  let seconds = 0;
+
+  const getNumbers = () => {
+    days = Math.floor(difference / 86400000);
+
+    hours = Math.floor((difference % 86400000) / 3600000);
+
+    minutes = Math.floor(((difference % 86400000) % 3600000) / 60000);
+
+    seconds = Math.floor((((difference % 86400000) % 3600000) % 60000) / 1000);
+  };
+
+  const renderZero = (value) => {
+    if (value < 10) return 0;
+  };
+
+  getNumbers();
+
+  return (
+    isClient && (
+      <div>
+        {renderZero(days)}
+        {days}:{renderZero(hours)}
+        {hours}:{renderZero(minutes)}
+        {minutes}:{renderZero(seconds)}
+        {seconds}
+      </div>
+    )
+  );
+};
+
+export default Counter;
--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,18 @@
-import Image from "next/image";
+import Counter from "./(components)/Counter";
 import Navigation from "./(components)/Navigation";
 
 const Home = () => {
   return (
-    <div>
+    <div className="main-content bg-center bg-cover bg-no-repeat">
       <nav>
         <Navigation />
       </nav>
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start"></main>
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <section id="header">
+          <h1>Jane's Birthday</h1>
+          <Counter />
+        </section>
+      </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
     </div>
   );
