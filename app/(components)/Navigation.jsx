939a3164c04a2e0e6dd0956544f5bb3e5d97f320
marta <m.nowak033@gmail.com>
2024-10-20 18:16:39 +0200

    Add mobile navigation

--- app/(components)/Navigation.jsx
+++ app/(components)/Navigation.jsx
@@ -1,21 +1,41 @@
-import Link from "next/link";
+"use client";
+
+import { useState } from "react";
 
 const Navigation = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <ul className="mx-10 mb-6 pt-6 flex gap-10 justify-end text-text font-instrument-sans">
-      <li className="nav-link">
-        <a href="#details">details</a>
-      </li>
-      <li className="nav-link">
-        <a href="#plan">plan</a>
-      </li>
-      <li className="nav-link">
-        <a href="#contact">contact</a>
-      </li>
-      <li className="nav-link">
-        <a href="#wishlist">wishlist</a>
-      </li>
-    </ul>
+    <div className="pt-5 pr-5 flex flex-col items-end">
+      <button
+        className="md:hidden flex flex-col items-end"
+        onClick={() => setIsOpen((prev) => !prev)}
+      >
+        <div className="border-2 border-text w-10 mb-1 rounded-full"></div>
+        <div className="border-2 border-text w-10 mb-1 rounded-full"></div>
+        <div className="border-2 border-text w-10 mb-1 rounded-full"></div>
+      </button>
+      <ul
+        className={`flex flex-col md:flex-row gap-10 justify-end md:mx-10 mb-6 py-4 md:py-0 px-4 md:px-0 text-text font-instrument-sans ${
+          isOpen
+            ? "absolute top-12 bg-primary md:bg-transparent rounded-xl"
+            : "hidden md:flex"
+        }`}
+      >
+        <li className="nav-link">
+          <a href="#details">details</a>
+        </li>
+        <li className="nav-link">
+          <a href="#plan">plan</a>
+        </li>
+        <li className="nav-link">
+          <a href="#contact">contact</a>
+        </li>
+        <li className="nav-link">
+          <a href="#wishlist">wishlist</a>
+        </li>
+      </ul>
+    </div>
   );
 };
 
