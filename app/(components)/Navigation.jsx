e779902ab3a2d17951abaaf930c28563dd170dff
marta <m.nowak033@gmail.com>
2024-11-05 14:50:12 +0100

    Alter mobile navigation design to give more space for user to click

--- app/(components)/Navigation.jsx
+++ app/(components)/Navigation.jsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useState } from "react";
+import close from "../close.svg";
+import Image from "next/image";
 
 const Navigation = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -16,23 +18,38 @@ const Navigation = () => {
         <div className="border-2 border-text w-10 mb-1 rounded-full"></div>
       </button>
       <ul
-        className={`flex flex-col md:flex-row gap-10 justify-end md:mx-10 mb-6 py-4 md:py-0 px-4 md:px-0 text-text font-instrument-sans ${
+        className={`flex flex-col md:flex-row gap-10 md:justify-end md:mx-10 mb-6 py-4 md:py-0 px-4 md:px-0 text-text font-instrument-sans ${
           isOpen
-            ? "absolute top-12 bg-primary md:bg-transparent rounded-xl"
+            ? "fixed md:static top-0 left-0 w-screen h-screen md:h-full md:w-full bg-primary md:bg-transparent items-center md:items-start"
             : "hidden md:flex"
         }`}
       >
+        {isOpen && (
+          <li className="block md:hidden self-end p-10">
+            <button onClick={() => setIsOpen(false)}>
+              <Image src={close} alt="close icon" width={30} height={30} />
+            </button>
+          </li>
+        )}
         <li className="nav-link">
-          <a href="#details">details</a>
+          <a href="#details" onClick={() => setIsOpen(false)}>
+            details
+          </a>
         </li>
         <li className="nav-link">
-          <a href="#plan">plan</a>
+          <a href="#plan" onClick={() => setIsOpen(false)}>
+            plan
+          </a>
         </li>
         <li className="nav-link">
-          <a href="#contact">contact</a>
+          <a href="#contact" onClick={() => setIsOpen(false)}>
+            contact
+          </a>
         </li>
         <li className="nav-link">
-          <a href="#wishlist">wishlist</a>
+          <a href="#wishlist" onClick={() => setIsOpen(false)}>
+            wishlist
+          </a>
         </li>
       </ul>
     </div>
